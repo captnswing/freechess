3c609c747816a46c0af09672cc4bd7c0104c4135
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-04-05 10:32:19 +0200

    improved settings.prod

--- freechess/settings/prod.py
+++ freechess/settings/prod.py
@@ -7,23 +7,12 @@ from .base import *
 # # database configuration
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES = dict()
-DATABASES['default'] = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# DATABASE_SUPPORTS_TRANSACTIONS = False
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'captnswing',
-#         'USER': 'root',
-#         'PASSWORD': 'mp109',
-#         'HOST': '',
-#         'PORT': '',
-#     }
-# }
 
 # debug
 DEBUG = False
