9873983500946e1ee0ced94e3aee8f54e28006e0
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-04-05 15:03:18 +0200

    removed fileupload app. starting over.

--- freechess/fileupload/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-from freechess.fileupload.views import PGNfileCreateView, PGNfileDeleteView
-
-urlpatterns = patterns('',
-    url(r'^new/$', PGNfileCreateView.as_view(), {}, name='upload-new'),
-    url(r'^delete/(?P<pk>\d+)$', PGNfileDeleteView.as_view(), {}, name='upload-delete'),
-)
-
--- freechess/urls.py
+++ freechess/urls.py
@@ -4,18 +4,13 @@ from django.views.generic.simple import direct_to_template
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# from http://www.arthurkoziel.com/2009/01/15/passing-mediaurl-djangos-500-error-view/
+
 handler500 = 'freechess.stats.util.server_error'
 
 # index
 urlpatterns = patterns('freechess.stats',
     url(r'^$', 'stats.chessStats', name='stats-index'),
     url(r'^pythonversion', 'util.pythonversion', name='stats-pythonversion'),
-)
-
-# data admin
-urlpatterns += patterns('freechess.fileupload',
-    url(r'^upload/', include('freechess.fileupload.urls')),
 )
 
 # api
