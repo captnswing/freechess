76a85ac922b05a3daea025351952384e6767aa1b
Frank Hoffsummer <frank.hoffsummer@gmail.com>
2013-04-05 10:00:24 +0200

    added wsgi, procfile, updated requirements.txt

---
+++ freechess/wsgi.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'freechess.settings.local'
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
